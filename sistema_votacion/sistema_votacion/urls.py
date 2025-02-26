bef9a9921032fe32ce7e854f0d6090c41815bddb
vanesa1212 <lgutierrezlozano09@gmail.com>
2025-02-25 21:24:35 -0500

    actualizacion de tarjetones

--- sistema_votacion/sistema_votacion/urls.py
+++ sistema_votacion/sistema_votacion/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from votaciones.views import template_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('votaciones/', template_view, name='votaciones'),
+
 ]
