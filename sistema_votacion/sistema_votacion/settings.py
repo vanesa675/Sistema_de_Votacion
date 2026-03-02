cae8214e43942ab7b979f68388366a797b40c4e3
Vanesa Lozano <lgutierrezlozano09@gmail.com>
2026-03-01 22:37:11 -0500

    ordenacion de grados

--- sistema_votacion/sistema_votacion/settings.py
+++ sistema_votacion/sistema_votacion/settings.py
@@ -128,6 +128,8 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Carpeta donde Django recolectará archivos estáticos en producción
 
 
--- sistema_votacion/votaciones/forms.py
+++ sistema_votacion/votaciones/forms.py
@@ -1,7 +1,17 @@
 from django import forms
-from .models import Estudiante
+from .models import Estudiante, grado
 
 class EstudianteForm(forms.ModelForm):
     class Meta:
         model = Estudiante
         fields = ['nombre', 'grado', 'mesa']
+
+class EstudianteForm(forms.ModelForm):
+
+    class Meta:
+        model = Estudiante
+        fields = ['nombre', 'grado', 'mesa']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['grado'].queryset = Grado.objects.order_by('numero')
